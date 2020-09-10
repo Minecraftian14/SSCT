65830a2484d1c618d9e56a3ab8a7a8af5e7a21f3
MCXIV <mcxiv14@gmail.com>
2020-09-10 15:00:58 +0530

    R CCE Bug

--- src/main/java/org/util/FieldIterator.java
+++ src/main/java/org/util/FieldIterator.java
@@ -9,23 +9,23 @@ import java.util.function.Consumer;
 
 public class FieldIterator {
 
-    private static HashMap<String, Field[]> map = new HashMap<>();
+    private static HashMap<String, HashSet<Field>> map = new HashMap<>();
 
     public static void foreach(Class<?> clazz, Consumer<Field> consumer) {
-        Field[] fields = getFrom(clazz);
+        HashSet<Field> fields = getFrom(clazz);
         for (Field field : fields) consumer.accept(field);
     }
 
-    private static Field[] getFrom(Class<?> clazz) {
+    private static HashSet<Field> getFrom(Class<?> clazz) {
         if (!map.containsKey(clazz.getName()))
             map.put(clazz.getName(), refine(clazz));
         return map.get(clazz.getName());
     }
 
-    private static Field[] refine(Class<?> clazz) {
+    private static HashSet<Field> refine(Class<?> clazz) {
         HashSet<Field> set = new HashSet<>(Arrays.asList(clazz.getDeclaredFields()));
         set.addAll(Arrays.asList(clazz.getFields()));
-        return (Field[]) set.toArray();
+        return set;
     }
 
 }
